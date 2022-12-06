69207be8a0f40348d006ab85413c87bbfe79bba3
conor-otoole <conor-otoole@users.noreply.github.com>
2022-12-06 21:21:28 +0000

    Prettified Code!

--- game.js
+++ game.js
@@ -7,23 +7,24 @@
 /* global phaser */
 
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: true
-    }
-  },    
+      debug: true,
+    },
+  },
   // set background color
   backgroundColor: 0x5f6e7a,
   scale: {
-    mode: Phaser.Scale,FIT,
+    mode: Phaser.Scale,
+    FIT,
     // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
-}
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);

aff455ccde5df953c039210461055ac33898b5fd
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-12-06 21:16:29 +0000

    hjgh

--- game.js
+++ game.js
@@ -1,20 +1,25 @@
-/* global Phaser */
+// Copyright (c) 2022 Conor All rights reserved
+//
+// Created by: conor-otoole
+// Created on: nov 2022
+// This is the file for Phaser3 configuration
 
-//* Game scene */
+/* global phaser */
+
 const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
+type: Phaser.AUTO,
+width: 1920,
+height: 1080,
   physics: {
     default: 'arcade',
     arcade: {
       debug: true
     }
-  },
+  },    
   // set background color
   backgroundColor: 0x5f6e7a,
   scale: {
-    mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale,FIT,
     // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
@@ -22,4 +27,3 @@ const config = {
 
 const game = new Phaser.Game(config)
 console.log(game)
-

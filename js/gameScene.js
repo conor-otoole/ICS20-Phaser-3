15e88d765800b1afee8d12724684175c628ee668
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-12-11 01:10:39 +0000

    Log

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,21 +7,35 @@
 // This is the Game scene
 
 class GameScene extends Phaser.Scene {
-  constructor() {
-    super({ key: "gameScene" });
+  constructor () {
+    super({ key: 'gameScene' })
+
+    this.background = null
+    this.ship = null
   }
 
-  init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff");
-  }
-
-  preload() {
-    console.log("Game Scene");
-  }
-
-  create(data) {}
-
-  update(time, delta) {}
+init (data) {
+  this.cameras.main.setBackgroundColor('#ffffff')
 }
 
-export default GameScene;
+  preload () {
+    console.log('Game Scene')
+
+    // images
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
+    
+  }
+
+  create (data) {
+      this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+  }
+
+  update (time, delta) {
+  }
+}
+
+export default GameScene

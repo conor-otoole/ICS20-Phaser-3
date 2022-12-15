66f1cbf8f8682dccdd945b22e6c42ede401ac056
conor-otoole <conor-otoole@users.noreply.github.com>
2022-12-15 19:31:21 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,52 +7,51 @@
 // This is the Game scene
 
 class GameScene extends Phaser.Scene {
-  constructor () {
-    super({ key: 'gameScene' })
+  constructor() {
+    super({ key: "gameScene" });
 
-    this.background = null
-    this.ship = null
+    this.background = null;
+    this.ship = null;
   }
 
-init (data) {
-  this.cameras.main.setBackgroundColor('#ffffff')
-}
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
 
-  preload () {
-    console.log('Game Scene')
+  preload() {
+    console.log("Game Scene");
 
     // images
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
-    
+    this.load.image("starBackground", "assets/starBackground.png");
+    this.load.image("ship", "assets/spaceShip.png");
   }
 
-  create (data) {
-      this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0, 0)
+  create(data) {
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
-  update (time, delta) {
+  update(time, delta) {
     // called 60 times a second, hopefully!
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
-    
+    const keyLeftObj = this.input.keyboard.addKey("LEFT");
+    const keyRightObj = this.input.keyboard.addKey("RIGHT");
+
     if (keyLeftObj.isDown === true) {
-      this.ship.x -= 15
+      this.ship.x -= 15;
       if (this.ship.x < 0) {
-        this.ship.x = 0
+        this.ship.x = 0;
       }
     }
-    
+
     if (keyRightObj.isDown === true) {
-      this.ship.x += 15
+      this.ship.x += 15;
       if (this.ship.x > 1920) {
-        this.ship.x = 1920
-      }  
+        this.ship.x = 1920;
+      }
     }
   }
 }
 
-export default GameScene
+export default GameScene;
